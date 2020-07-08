4936c4afe76e20b0c767c36e2ee8f8eeb91e9dce
micswe <58488062+micswe@users.noreply.github.com>
2020-07-08 18:56:47 +0200

    Hydrate Script with Work Hours

--- Framework/SubCall/Personalization/10_PersBISF_HydratePVS.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_HydratePVS.ps1
@@ -8,55 +8,97 @@
     .Inputs
     .Outputs
     .NOTES
+      Author: Trentent Tye
 
       History
 		  2019.08.16 TT: Script created
 		  18.08.2019 MS: integrate into BIS-F
-		  03.02.2020 MS: HF 201 - Hydration not startig if configured
-		  23.05.2020 MS: HF 231 - Skipping file precache if vDisk is in private Mode
+		  01.04.2020 Micswe: Add PVS Disk mode
+		  20.04.2020 Micswe: Add Work Hours, Skip Hydrate on boot in WorkHourse
+		  24.04.2020 Micswe: BugFix hang on many Clients
+
 	  .Link
 		  https://github.com/EUCweb/BIS-F/issues/129
 
 	  .Link
 		  https://eucweb.com
-    #>
+#>
 
 Begin {
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	Write-BISFLog -Msg "Hydrate Begin"
+	#$script_path = $MyInvocation.MyCommand.Path
+	#$script_dir = Split-Path -Parent $script_path
+	#$script_name = [System.IO.Path]::GetFileName($script_path)
+	if ($LIC_BISF_CLI_PVSHydration -eq "YES") { $EnableMode = $true }
+	if ($LIC_BISF_CLI_PVSHydration -eq "NO") { $DisableMode = $true }
 	$PathsToCache = $LIC_BISF_CLI_PVSHydration_Paths
 	$ExtensionsToCache = $LIC_BISF_CLI_PVSHydration_Extensions
+	[int]$only_runfrom=17
+	[int]$only_runto=5
+	[bool]$check_Work_Hour=$true
 }
 
 Process {
-
-
+	####################################################################
+	####### Functions #####
+	####################################################################
 	function FileToCache ($File) {
 		#Write-BISFLog -Msg "Caching File : $File" -ShowConsole -Color Cyan
 		$hydratedFile = [System.IO.File]::ReadAllBytes($File)
 	}
+  	####################################################################
+	####### End functions #####
+	####################################################################
 
-	$WriteCacheType = Get-BISFPVSWriteCacheType
-	if ($WriteCacheType -eq 0) {   # private Mode
-		Write-BISFLog -Msg "PVS vDisk is in Private Mode. Skipping file precache."  -ShowConsole -Color Yellow
-		Return
-	}
+	Write-BISFLog -Msg "Hydrate Prozess Start"
 
-	if (-not(Test-BISFPVSSoftware)) {
-		Write-BISFLog -Msg "PVS Software not found. Skipping file precache."  -ShowConsole -Color Yellow
-		Return
+	$WriteCacheType = (Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\bnistack\PVSAgent).WriteCacheType
+	Switch ($WriteCacheType)
+	{
+		0 {$WriteCacheTypeTxt = "Private"}
+		1 {$WriteCacheTypeTxt = "Cache on Server"}
+		3 {$WriteCacheTypeTxt = "Cache in Device RAM"}
+		4 {$WriteCacheTypeTxt = "Cache on Device Hard Disk"}
+		7 {$WriteCacheTypeTxt = "Cache on Server, Persistent"}
+		9 {$WriteCacheTypeTxt = "Cache in Device RAM with Overflow on Hard Disk"}
+		10 {$WriteCacheTypeTxt = "Private async"}
+		11 {$WriteCacheTypeTxt = "Server persistent async"}
+		12 {$WriteCacheTypeTxt = "Cache in Device RAM with Overflow on Hard Disk async"}
+		default {$WriteCacheTypeTxt = "WriteCacheType $WriteCacheType not defined !!"}
 	}
-	if (-not($LIC_BISF_CLI_PVSHydration -eq "YES")) {
-		Write-BISFLog -Msg "File precache configuration not found. Skipping."  -ShowConsole -Color Yellow
-		Return
-	}
+	Write-BISFLog -Msg "PVS WriteCacheType is set to $WriteCacheType - $WriteCacheTypeTxt"
 
-	foreach ($Path in ($PathsToCache.split("|"))) {
-		Write-BISFLog -Msg "Caching files with extensions $ExtensionsToCache in $Path" -ShowConsole -Color Cyan
-		foreach ($File in (Get-ChildItem -Path $Path -Recurse -File -Include $ExtensionsToCache.Split(","))) {
-			FileToCache -File $File
+	if ($WriteCacheType -eq 0) {   # 4:Cache on Device Hard Disk // 9:Cache in Device RAM with Overflow on Hard Disk // 12:Cache in Device RAM with Overflow on Hard Disk async
+		Write-BISFLog -Msg "PVS Software is in Master mode, skip precache."  -ShowConsole -Color Yellow
+		Return	
+	}	
+	
+	#check time
+	[int]$time_hour=(Get-Date -Format HH)
+
+	if (($time_hour -ge $only_runfrom -or $time_hour -le $only_runto) -or $check_Work_Hour -eq $false)
+	{
+		Write-BISFLog -Msg "Start Hydrate in RunHours: Yes"  -ShowConsole -Color Yellow
+
+		if (-not(Test-BISFPVSSoftware)) {
+			Write-BISFLog -Msg "PVS Software not found. Skipping file precache."  -ShowConsole -Color Yellow
+			Return
 		}
+		if (-not($EnableMode) -or ($DisableMode)) {
+			Write-BISFLog -Msg "File precache configuration not found. Skipping."  -ShowConsole -Color Yellow
+			Return
+		}
+
+		foreach ($Path in ($PathsToCache.split("|"))) {
+			Write-BISFLog -Msg "Caching files with extensions $ExtensionsToCache in $Path" -ShowConsole -Color Cyan
+			foreach ($File in (Get-ChildItem -Path $Path -Recurse -File -Include $ExtensionsToCache.Split(","))) {
+				FileToCache -File $File
+			}
+		}
+	}
+	else
+	{
+		Write-BISFLog -Msg "Skype Hydrate, Start in Work hours."  -ShowConsole -Color Yellow
 	}
 }
 
